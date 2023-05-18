67d8366af63e5aa03a4c2cbe884ea5645cd6c547
Josue2537182019 <126131225+Josue2537182019@users.noreply.github.com>
2023-05-17 21:57:52 -0600

    parcial4

---
+++ app/src/main/java/com/example/parcial4/CRUD.java
@@ -0,0 +1,11 @@
+package com.example.parcial4;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class CRUD {
+    public SQLiteDatabase getCone(Context context)
+    {
+
+    }
+}
---
+++ app/src/main/java/com/example/parcial4/consqlite.java
@@ -0,0 +1,33 @@
+package com.example.parcial4;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class consqlite extends SQLiteOpenHelper {
+
+    final String tblMD_Clientes = "CREATE TABLE MD_Clientes (ID_Cliente INTEGER PRIMARY KEY, sNombreCliente TEXT, sApellidosClientes TEXT, sDireccionClientes TEXT, sCiudadCliente TEXT)";
+    final String tblMD_ClienteVehiculo = "CREATE TABLE MD_ClienteVehiculo (ID_Cliente INTEGER, ID_Vehiculo INTEGER, sMatricula TEXT, iKilometros INTEGER, " +
+            "PRIMARY KEY (ID_Cliente, ID_Vehiculo), " +
+            "FOREIGN KEY (ID_Cliente) REFERENCES MD_Clientes(ID_Cliente), " +
+            "FOREIGN KEY (ID_Vehiculo) REFERENCES MD_Vehiculo(ID_Vehiculo))";
+    final String tblMD_Vehiculos= "CREATE TABLE MD_Vehiculo (ID_Vehiculo INTEGER PRIMARY KEY, sMarca TEXT, sModelo TEXT)";
+
+    public consqlite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(tblMD_Clientes);
+        db.execSQL(tblMD_ClienteVehiculo);
+        db.execSQL(tblMD_Vehiculos);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // Aquí puedes realizar la migración de datos si es necesario
+    }
+}
